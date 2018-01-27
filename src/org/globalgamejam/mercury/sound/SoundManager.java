1343eadb212ffcdda2e644c2353497bc9999c783
Bradley Shrader <bkshrader@bsu.edu>
2018-01-27 18:57:33 -0500

    Add Sound Manager
    Does not implement the sound manager into the workflow

---
+++ src/org/globalgamejam/mercury/sound/SoundManager.java
@@ -0,0 +1,22 @@
+/*
+ * This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/ or send a letter to
+ * Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ */
+
+package org.globalgamejam.mercury.sound;
+
+public class SoundManager {
+    private static SoundManager ourInstance = new SoundManager();
+
+    private SoundManager() {
+    }
+
+    public static SoundManager getInstance() {
+        return ourInstance;
+    }
+
+    public void init() {
+
+    }
+}
